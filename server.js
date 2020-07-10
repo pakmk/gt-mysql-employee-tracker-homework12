3d89b48f8a5550bf4c679b9ad72b67e0f973d911
min <pakmk8@gmail.com>
2020-07-09 22:10:44 -0400

    added inquirer questions

--- server.js
+++ server.js
@@ -13,5 +13,50 @@ const connection = mysql.createConnection({
 connection.connect((err) => {
   if (err) throw err;
   console.log("Connected as ID " + connection.threadId);
-    // init();
 });
+
+const questions = [
+  {
+    type: "list",
+    message: "What would you like to do?",
+    name: "view",
+    choices: [
+      {
+        name: "View all Employees",
+        value: "viewEmployees"
+      },
+      {
+        name: "View all Employees By Department",
+        value: "viewAll"
+      },
+      {
+        name: "View all Employees By Manager",
+        value: "viewManager"
+      },
+      {
+        name: "Add Employee",
+        value: "addEmployee"
+      },
+      {
+        name: "Remove Employee",
+        value: "removeEmployee"
+      },
+      {
+        name: "Update Employee Role",
+        value: "updateEmployee"
+      },
+      {
+        name: "Update Employee Manager",
+        value: "updateEmployeeManager"
+      },
+    ],
+  },
+];
+
+function init() {
+  inquirer.prompt(questions).then((response) => {
+  });
+}
+
+init();
+
