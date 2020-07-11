9ee2de0301323765b67c9b505aac56d772341b13
min <pakmk8@gmail.com>
2020-07-10 22:41:12 -0400

    updated server.js

--- server.js
+++ server.js
@@ -29,27 +29,67 @@ const questions = [
         name: "View all Employees By Department",
         value: "viewAll",
       },
-      {
-        name: "View all Employees By Manager",
-        value: "viewManager",
-      },
+      // {
+      //   name: "View all Employees By Manager",
+      //   value: "viewManager",
+      // },
       {
         name: "Add Employee",
         value: "addEmployee",
       },
-      {
-        name: "Remove Employee",
-        value: "removeEmployee",
-      },
+      // {
+      //   name: "Remove Employee",
+      //   value: "removeEmployee",
+      // },
       {
         name: "Update Employee Role",
         value: "updateEmployee",
       },
+      // {
+      //   name: "Update Employee Manager",
+      //   value: "updateEmployeeManager",
+      // },
       {
-        name: "Update Employee Manager",
-        value: "updateEmployeeManager",
+        name: "View Department",
+        value: "viewDept",
+      },
+      {
+        name: "View Role",
+        value: "viewRole",
+      },
+      {
+        name: "View Employee",
+        value: "viewEmpl",
+      },
+      {
+        name: "Exit",
+        value: "exit",
       },
     ],
+  },
+];
+
+const addEmpl = [
+  {
+    type: "input",
+    message: "What is the employee's first name?",
+    name: "first_name",
+  },
+  {
+    type: "input",
+    message: "What is the employee's last name?",
+    name: "last_name",
+  },
+  {
+    type: "list",
+    message: "What is the employee's role?",
+    name: "role_id",
+    choices: ["Engineering", "Finance", "Sales", "Legal"],
+  },
+  {
+    type: "input",
+    message: "Who is the employee's manager?",
+    name: "manager_id",
   },
 ];
 
@@ -58,17 +98,24 @@ function init() {
     if (response.view === "viewEmployees") {
       allEmployees();
     } else if (response.view === "viewAll") {
-      allEmployeesByDepartment(); 
-    } else if (response.view === "viewManager"){
-      allEmployeesByManager();
-    }
-   
-
+      allEmployeesByDepartment();
+    // } else if (response.view === "viewManager") {
+      // allEmployeesByManager();
+    } else if (response.view === "addEmployee") {
+      addEmployee();
+    } else if (response.view === "viewDept") {
+      viewDept();
+    } else if (response.view === "viewRole") {
+      viewRole();
+    } else if (response.view === "viewEmpl") {
+      viewEmpl();
+    } else (response.view === "exit") 
+      exit();
   });
 }
 function allEmployees() {
   connection.query(
-    "SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary FROM employee LEFT JOIN role ON employee.role_id = role.id",
+    "SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary FROM employee LEFT JOIN role ON employee.role_id = role.id;",
     (err, res) => {
       if (err) throw err;
       console.table(res);
@@ -79,7 +126,7 @@ function allEmployees() {
 
 function allEmployeesByDepartment() {
   connection.query(
-    "SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary FROM employee LEFT JOIN role ON employee.role_id = role.id",
+    "SELECT employee.id, department.name, employee.first_name, employee.last_name, role.title, role.salary, employee.manager_id FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id;",
     (err, res) => {
       if (err) throw err;
       console.table(res);
@@ -88,9 +135,29 @@ function allEmployeesByDepartment() {
   );
 }
 
-function allEmployeesByManager() {
+// function allEmployeesByManager() {
+//   connection.query("",
+//     (err, res) => {
+//       if (err) throw err;
+//       console.table(res);
+//       init();
+//     }
+//   );
+// }
+
+function addEmployee() {
+  inquirer.prompt(addEmpl).then((response) => {
+    // console.log(response);
+    connection.query(`INSERT INTO employee SET ?`, response, (err) => {
+      if (err) throw err
+      init();
+    });
+  });
+}
+
+function viewDept() {
   connection.query(
-    "SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary FROM employee LEFT JOIN role ON employee.role_id = role.id",
+    "SELECT * FROM department;",
     (err, res) => {
       if (err) throw err;
       console.table(res);
@@ -99,6 +166,30 @@ function allEmployeesByManager() {
   );
 }
 
+function viewRole() {
+  connection.query(
+    "SELECT * FROM role;",
+    (err, res) => {
+      if (err) throw err;
+      console.table(res);
+      init();
+    }
+  );
+}
 
+function viewEmpl() {
+  connection.query(
+    "SELECT * FROM employee;",
+    (err, res) => {
+      if (err) throw err;
+      console.table(res);
+      init();
+    }
+  );
+}
+
+function exit() {
+  connection.end();
+}
 
 init();
